63aad3839dc6c7e39f1019baa7ae34038d1e71f6
Yiding He <yhe@truxnow.com>
2019-05-03 15:56:42 -0400

    Updated gitignore to add android. Stful and Stless.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_travel/demo/less_group_page.dart';
+import 'package:flutter_travel/demo/stateful_group_page.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(StatefulGroup());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
