61228a46c195e79965dfb722ecdb550eab899181
Yiding He <yhe@truxnow.com>
2019-05-08 16:48:04 -0400

    Added url_launcher.

--- lib/demo/demo_main.dart
+++ lib/demo/demo_main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_travel/demo/flutter_layout_page.dart';
 import 'package:flutter_travel/demo/gesture_page.dart';
+import 'package:flutter_travel/demo/launch_page.dart';
 import 'package:flutter_travel/demo/less_group_page.dart';
 import 'package:flutter_travel/demo/stateful_group_page.dart';
 
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
           'layout': (BuildContext context) => FlutterLayoutPage(),
           'gesture': (BuildContext context) => GesturePage(),
 //          'res': (BuildContext context) => ResPage(),
-//          'launch': (BuildContext context) => LaunchPage(),
+          'launch': (BuildContext context) => LaunchPage(),
 //          'photo': (BuildContext context) => PhotoApp(),
         });
   }
@@ -59,7 +60,7 @@ class _RouteNavigatorState extends State<RouteNavigator> {
           _item('如何进行Flutter布局开发', FlutterLayoutPage(), 'layout'),
           _item('如何检测用户手势以及处理点击事件？', GesturePage(), 'gesture'),
 //          _item('如何导入和使用Flutter的资源文件？', ResPage(), 'res'),
-//          _item('如何打开第三方应用？', LaunchPage(), 'launch'),
+          _item('如何打开第三方应用？', LaunchPage(), 'launch'),
 //          _item('【实战尝鲜】拍照APP开发', PhotoApp(), 'photo'),
         ],
       ),
