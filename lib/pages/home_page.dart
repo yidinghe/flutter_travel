6208521de6f0dfe34640e91b4e3c3203f573886c
Yiding He <yidinghe2012@gmail.com>
2019-06-05 21:00:09 -0400

    5-4, Added banner.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,11 +7,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List _imageUrls = [
+    'http://images6.fanpop.com/image/photos/40400000/One-Piece-maouki-40476927-2560-1440.jpg',
+    'http://images6.fanpop.com/image/photos/40400000/Fairy-Tail-maouki-40449790-1920-1080.jpg',
+    'http://images6.fanpop.com/image/photos/40400000/Fairy-Tail-maouki-40449793-1920-1080.jpg'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('首页'),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 160,
+              child: Swiper(
+                itemCount: _imageUrls.length,
+                autoplay: true,
+                itemBuilder: (BuildContext context, int index) {
+                  return Image.network(
+                    _imageUrls[index],
+                    fit: BoxFit.fill,
+                  );
+                },
+                pagination: SwiperPagination(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
